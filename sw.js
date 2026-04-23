30ba7210c5b352494de02abd8ac806038a7a0ffe
lavipco <mvulam@gmail.com>
2026-04-23 22:46:11 +0700

    PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'lighting-system-v1.1';
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  'css/bootstrap.min.css',
+  'css/style.css',
+  'css/responsive.css',
+  'css/jquery.mCustomScrollbar.min.css',
+  'css/owl.carousel.min.css',
+  'css/owl.theme.default.min.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css',
+  'https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.1/exceljs.min.js',
+  'images/fevicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
